3a233736470ec0d8f24c2f9cf347faba12bf366e
Tharindi Bandara <tharinibandara123@gmail.com>
2024-05-18 16:28:35 +0530

    stage

---
+++ main.py
@@ -0,0 +1,15 @@
+
+# main.py
+
+# making objects of animals
+from dog import Dog
+from cat import Cat
+
+def main():
+    dog = Dog("Buddy")
+    cat = Cat("Whiskers")
+    print(dog.speak())
+    print(cat.speak())
+
+if __name__ == "__main__":
+    main()
