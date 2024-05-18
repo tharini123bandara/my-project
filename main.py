cf6aab678a4460b27b801ea068d98cc7d66ff906
Tharindi <152827891+tharini123bandara@users.noreply.github.com>
2024-05-18 20:40:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,15 +2,19 @@
 
 from bird import Bird
 from fish import Fish
+from elephant import Elephant
 
 def main():
    
     bird = Bird("Tweety")
     fish = Fish("Nemo")
-
+    elephant = Elephant("Dumbo")
+     
     
     print(bird.speak())
     print(fish.speak())
+    print(elephant.speak())
+
 
 if __name__ == "__main__":
     main()
