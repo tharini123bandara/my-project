d69b98c1d25338c9acecafd0d59597802a0c0ebe
Tharindi Bandara <tharinibandara123@gmail.com>
2024-05-18 16:30:31 +0530

    deleting another files

--- animal.py
+++
@@ -1,7 +0,0 @@
-# animal.py
-class Animal:
-    def __init__(self, name):
-        self.name = name
-
-    def speak(self):
-        raise NotImplementedError("Subclass must implement abstract method")
